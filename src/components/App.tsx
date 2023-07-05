a0e980d5c00b43554ff490dae96352ca1aa40a2f
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-05 17:16:33 +0100

    Add baby names and styling
    display: flex;

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import { useMemo } from "react";
+import babyNames from "../babyNamesData.json";
+import { BabyNameCard } from "./babyName";
+import { sortNames } from "../utils/sortNames";
 
 function App() {
+  const babyNamesAll = useMemo(() => sortNames(babyNames), []);
+
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <div className="App-container">
+      <div className="App">
+        <div className="divider"></div>
+        <section className="name-container">
+          {babyNamesAll.map(({ id, name, sex }) => {
+            return <BabyNameCard key={id} name={name} sex={sex} />;
+          })}
+        </section>
+        <div className="divider"></div>
+      </div>
     </div>
   );
 }
