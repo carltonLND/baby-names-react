42fb16412364c0a1c7e57a91854e0189ccaa9fdc
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-06 18:03:11 +0100

    Add onClick handler for favoriting

--- src/components/babyName.tsx
+++ src/components/babyName.tsx
@@ -1,8 +1,25 @@
 interface BabyNameCardProps {
+  id: number;
   name: string;
   sex: string;
+  onClick(): void;
 }
 
-export function BabyNameCard({ name, sex }: BabyNameCardProps): JSX.Element {
-  return <div className={`name-card sex-${sex}`}>{name}</div>;
+export function BabyNameCard({
+  id,
+  name,
+  sex,
+  onClick,
+}: BabyNameCardProps): JSX.Element {
+  return (
+    <div
+      onClick={onClick}
+      onKeyDown={onClick}
+      tabIndex={id}
+      role="button"
+      className={`name-card sex-${sex}`}
+    >
+      {name}
+    </div>
+  );
 }
